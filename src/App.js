8a9af49b1ad9f64e54d40a62d967a9032f973006
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2024-01-09 14:33:36 +0530

    add the hover on the book
    need to fix the hover to work properly

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [searchString, setSearchString] = useState("");
   const [debouncedSearchString, setDebouncedSearchString] = useState("");
   const [offset, setOffset] = useState(0);
+  const [showAuthor, setShowAuthor] = useState(false);
 
   const fetchBooks = async (query) => {
     try {
@@ -48,17 +49,32 @@ function App() {
     setSearchString(e.target.value);
   };
 
+  // need to work on this
+  const onHover = () => {
+    setShowAuthor(true);
+  };
+
   return (
     <div>
       <input className="searchBox" onChange={onChange} />
 
       <div className="booksList">
         {books.map((book) => {
-          const { isbn, id, title } = book;
-          const key = "isbn";
-          const value = isbn[0];
+          const { isbn, oclc, lccn, id, title } = book;
+          let key = "";
+          let value = "";
+          if (isbn && isbn.length > 0) {
+            key = "isbn";
+            value = isbn[0];
+          } else if (oclc && oclc.length > 0) {
+            key = "oclc";
+            value = oclc[0];
+          } else if (lccn && lccn.length > 0) {
+            key = "lccn";
+            value = lccn[0];
+          }
           return (
-            <div key={id} className="bookContainer">
+            <div key={id} className="bookContainer" onMouseEnter={onHover}>
               <img
                 className="bookImage"
                 src={`https://covers.openlibrary.org/b/${key}/${value}-S.jpg`}
