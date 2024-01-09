8e532179077d8994aed49113653194fe5a22091a
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2024-01-09 14:23:33 +0530

    add images of the books

--- src/App.js
+++ src/App.js
@@ -3,38 +3,80 @@ import { useEffect, useState } from "react";
 
 const url = "https://openlibrary.org/search.json";
 
+const limit = 20;
+
 function App() {
   const [books, setBooks] = useState([]);
   const [searchString, setSearchString] = useState("");
+  const [debouncedSearchString, setDebouncedSearchString] = useState("");
+  const [offset, setOffset] = useState(0);
 
   const fetchBooks = async (query) => {
     try {
-      const response = await fetch(`${url}?q=${query}`);
-      const { numFound, docs, start } = await response.json();
-      setBooks((prev) => [...prev, ...docs]);
+      const response = await fetch(
+        `${url}?q=${query}&offset=${offset}&limit=${limit}`,
+      );
+      const { docs } = await response.json();
+      setBooks((prev) => {
+        if (offset) {
+          return [...prev, ...docs];
+        }
+        return docs;
+      });
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    if (searchString) {
-      const query = searchString.split(" ").join("+");
-      fetchBooks(query);
-    }
+    const delayInputTimeoutId = setTimeout(() => {
+      setDebouncedSearchString(searchString);
+    }, 500);
+    return () => clearTimeout(delayInputTimeoutId);
   }, [searchString]);
+
+  useEffect(() => {
+    if (debouncedSearchString) {
+      const query = debouncedSearchString.split(" ").join("+");
+      fetchBooks(query);
+    } else {
+      setBooks([]);
+    }
+  }, [debouncedSearchString]);
 
   const onChange = (e) => {
     setSearchString(e.target.value);
   };
 
   return (
-    <div className="App">
-      <input onChange={onChange} />
+    <div>
+      <input className="searchBox" onChange={onChange} />
 
-      {books.map((book) => (
-        <div key={book.id}>{book.title}</div>
-      ))}
+      <div className="booksList">
+        {books.map((book) => {
+          const { isbn, id, title } = book;
+          const key = "isbn";
+          const value = isbn[0];
+          return (
+            <div key={id} className="bookContainer">
+              <img
+                className="bookImage"
+                src={`https://covers.openlibrary.org/b/${key}/${value}-S.jpg`}
+                alt={title}
+              />
+              {showAuthor && (
+                <div className="bookAuthor">
+                  <img
+                    className="bookAuthor"
+                    src={`https://covers.openlibrary.org/a/${key}/${value}-$size.jpg`}
+                    alt={title}
+                  />
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
