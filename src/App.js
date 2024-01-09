b34ee43ea5894090d16ea986ab69ddff645b681a
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2024-01-09 13:43:35 +0530

    add input and api call to fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const url = "https://openlibrary.org/search.json";
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [searchString, setSearchString] = useState("");
+
+  const fetchBooks = async (query) => {
+    try {
+      const response = await fetch(`${url}?q=${query}`);
+      const { numFound, docs, start } = await response.json();
+      setBooks((prev) => [...prev, ...docs]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (searchString) {
+      const query = searchString.split(" ").join("+");
+      fetchBooks(query);
+    }
+  }, [searchString]);
+
+  const onChange = (e) => {
+    setSearchString(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={onChange} />
+
+      {books.map((book) => (
+        <div key={book.id}>{book.title}</div>
+      ))}
     </div>
   );
 }
